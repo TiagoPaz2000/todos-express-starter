5261dc7fcb3d4c26449347183aeb1f58c1e8f06d
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-26 19:54:21 -0300

    add: google strategy

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const GoogleStrategy = require('passport-google-oidc');
 const crypto = require('crypto');
 const db = require('../db');
 
@@ -16,6 +17,47 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
       }
       return cb(null, row);
     });
+  });
+}));
+
+passport.use(new GoogleStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/google',
+  scope: [ 'profile' ]
+}, function verify(issuer, profile, cb) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    issuer,
+    profile.id
+  ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) {
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+
+        const id = this.lastID;
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          issuer,
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          const user = {
+            id: id,
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
+      });
+    }
   });
 }));
 
@@ -38,6 +80,13 @@ router.post('/login/password', passport.authenticate('local', {
   failureRedirect: '/login'
 }));
 
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
@@ -54,7 +103,7 @@ router.get('/signup', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next) {
-  var salt = crypto.randomBytes(16);
+  const salt = crypto.randomBytes(16);
   crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
     if (err) { return next(err); }
     db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
@@ -63,7 +112,7 @@ router.post('/signup', function(req, res, next) {
       salt
     ], function(err) {
       if (err) { return next(err); }
-      var user = {
+      const user = {
         id: this.lastID,
         username: req.body.username
       };
